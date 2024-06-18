bd014bf301613d66147e1be913c8257a9dc183b2
ani8532 <141420423+ani8532@users.noreply.github.com>
2024-06-18 22:27:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
-var randomiceimage1 = "images/dice" + randomNumber1 + ".png";
+var randomiceimage1 = "dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src",randomiceimage1);
 
 
 var randomNumber2 = Math.floor(Math.random()*6) + 1;
-var randomdiceimage2 = "images/dice" + randomNumber2 + ".png";
+var randomdiceimage2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomdiceimage2);
 
 if (randomNumber1 > randomNumber2) {
